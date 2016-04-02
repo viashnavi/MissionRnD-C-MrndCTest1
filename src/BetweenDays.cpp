e579a0d5ec306d8156701f5701c902f43b89b819
viashnavi <viashnavi@missionrnd.com>
2016-04-02 09:11:15 -0700

    Submit Commit

--- src/BetweenDays.cpp
+++ src/BetweenDays.cpp
@@ -28,7 +28,82 @@ struct node{
 	struct node *next;
 };
 
+int month(int a, int yy)
+{
+	int mon[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	int x = 0, c;
+	for (c = 0; c<a - 1; c++)
+	{
+		if (c == 1)
+		{
+			if (yy % 4 == 0)
+				x += 29;
+			else
+				x += 28;
+		}
+		else
+			x += mon[c];
+	}
+	return(x);
+}
+
+int days(int y1, int y2, int m1, int m2, int d1, int d2)
+{
+	int count = 0, i;
+	for (i = y1; i<y2; i++)
+	{
+		if (i % 4 == 0)
+			count += 366;
+		else
+			count += 365;
+	}
+	count -= month(m1, y1);
+	count -= d1;
+	count += month(m2, y2);
+	count += d2;
+	if (count<0)
+		count = count*-1;
+	return count - 1;
+}
+
 
 int between_days(struct node *date1head, struct node *date2head){
+	if (date1head != NULL && date2head != NULL){
+		int y1 = 0, y2 = 0, m1 = 0, m2 = 0, d1 = 0, d2 = 0;
+		int count = 0;
+		// 0->1->0->1->2->0->0->4 .
+		while (date1head != NULL && count != 2){
+			d1 = (d1 * 10) + (date1head->data);
+			date1head = date1head->next;
+			count++;
+		}
+		while (date1head != NULL && count != 4){
+			m1 = (m1 * 10) + (date1head->data);
+			date1head = date1head->next;
+			count++;
+		}
+		while (date1head != NULL && count != 8){
+			y1 = (y1 * 10) + (date1head->data);
+			date1head = date1head->next;
+			count++;
+		}
+		count = 0;
+		while (date2head != NULL && count != 2){
+			d2 = (d2 * 10) + (date2head->data);
+			date2head = date2head->next;
+			count++;
+		}
+		while (date2head != NULL && count != 4){
+			m2 = (m2 * 10) + (date2head->data);
+			date2head = date2head->next;
+			count++;
+		}
+		while (date2head != NULL && count != 8){
+			y2 = (y2 * 10) + (date2head->data);
+			date2head = date2head->next;
+			count++;
+		}
+		return days(y1, y2, m1, m2, d1, d2);
+	}
 	return -1;
 }
